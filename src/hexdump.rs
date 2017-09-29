83eb42d8c42537286433e9ef5b2626ff06d3da73
Gilad Naaman <gilad.naaman@gmail.com>
2017-09-29 12:24:49 +0300

    Added the Hexdump utility struct

---
+++ src/hexdump.rs
@@ -0,0 +1,127 @@
+use core::fmt::*;
+
+
+/// A formatting aid for pretty-printing a byte buffer,
+/// not unlike the `hexdump` utility.
+///
+/// # Examples
+/// ```
+/// use extfmt::*;
+///
+/// let dump = Hexdump::new(&[1,2,3,4,5,6,255]);
+/// assert_eq!(format!("{}", dump), "00000000\t01 02 03 04 05 06 ff");
+/// ```
+pub struct Hexdump<'a> {
+	data: &'a [u8],
+	show_index: bool,
+	items_per_row: usize, 
+}
+
+impl<'a> Hexdump<'a> {
+	/// Creates a new Hexdump instance from a byte slice.
+	pub fn new(data: &'a [u8]) -> Self {
+		Hexdump {
+			data,
+			show_index: true,
+			items_per_row: 16
+		}
+	}
+
+	/// Controls whether or not to show the current index at the beginning of each row.
+	///
+	/// Default: true
+	pub fn show_index(&mut self, value: bool) -> &mut Self {
+		self.show_index = value;
+		self
+	}
+
+	/// Controls the amount of bytes to print in each row.
+	///
+	/// Default: 16
+	pub fn items_per_row(&mut self, value: usize) -> &mut Self {
+		self.items_per_row = value;
+		self
+	}
+}
+
+impl<'a> Display for Hexdump<'a> {
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		for i in 0..self.data.len() {
+			if self.show_index && i % self.items_per_row == 0 {
+				write!(f, "{:08x}\t", i)?;
+			}
+
+			write!(f, "{:02x}", self.data[i])?;
+
+			// Items separator
+			if i != self.data.len() - 1 {
+				// Start a new row when appropriate
+				if i % self.items_per_row == self.items_per_row - 1 {
+					f.write_char('\n')?;
+				}
+				else {
+					f.write_char(' ')?;
+				}
+			}
+		}
+
+		Ok(())
+	}
+}
+
+/// A utility trait used to create Hexdump objects.
+pub trait AsHexdump {
+	fn as_hexdump(&self) -> Hexdump;
+}
+
+impl<T: Sized> AsHexdump for T {
+	fn as_hexdump(&self) -> Hexdump {
+		let slice = unsafe { ::core::slice::from_raw_parts(
+    			self as *const _ as *const u8,
+    			::core::mem::size_of_val(self)
+		)};
+		
+    	Hexdump::new(slice)
+	}
+}
+
+impl AsHexdump for [u8] {
+	fn as_hexdump(&self) -> Hexdump {
+    	Hexdump::new(self)
+	}
+}
+
+/// Create a hexdump of the given value using the AsHexdump trait.
+/// It is implemented by default for Sized types and for [u8].
+///
+/// `key: value` can be added to control formatting options, where `key` is a method of the `Hexdump` struct.
+///
+/// # Examples
+/// ```
+/// use extfmt::*;
+/// let data = &[1u8,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+/// let expected = "00000000\t01 02 03 04 05 06 07 08 09 0a 0b 0c\n0000000c\t0d 0e 0f 10";
+/// assert_eq!(format!("{}", hexdump!(data, items_per_row: 12)), expected);
+/// ```
+#[macro_export]
+macro_rules! hexdump {
+    ($value:expr) => (
+    	$value.as_hexdump()
+    );
+    ($value:expr, $($setting:ident: $setting_value: expr)*) => (
+    	$value.as_hexdump() $(
+    		.$setting($setting_value)
+    		)*
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    	let data = &[1u8,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+        println!("{}", hexdump!(data, items_per_row: 12));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,108 +2,18 @@
 
 extern crate core;
 
-use core::fmt::*;
+mod slice;
+mod hexdump;
 
-/// Format a slice in a Rust syntax.
-/// Supports formatting using the following traits:
-///		- Display
-///		- Debug
-///		- Binary
-///		- Octal
-///		- LowerHex, UpperHex
-///		- LowerExp, UpperExp
-///		- Pointer
-///
-/// # Examples
-/// ```
-/// use extfmt::*;
-///
-/// assert_eq!(format!("{}", CommaSeparated(&[1, 2, 3])), "[1, 2, 3]");
-/// assert_eq!(format!("{:x}", CommaSeparated(&[122, 123, 134])), "[7a, 7b, 86]");
-/// assert_eq!(format!("{:#o}", CommaSeparated(&[122, 123, 134])), "[0o172, 0o173, 0o206]");
-/// ```
-#[derive(Clone)]
-pub struct CommaSeparated<'a, T: 'a>(pub &'a [T]);
+pub use slice::*;
+pub use hexdump::*;
 
-macro_rules! additional_slice_formatting {
-    ($($tr:ident),*) => {
-        $(
-        impl<'a, T: 'a + $tr> $tr for CommaSeparated<'a, T> {
-            fn fmt(&self, f: &mut Formatter) -> Result {
-                let mut has_items = false;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-                f.write_str("[")?;
-
-                for i in self.0.iter() {
-                    if has_items {
-                        f.write_str(", ")?;
-                    }
-
-                    <T as $tr>::fmt(i, f)?;
-                    has_items = true;
-                }
-
-                f.write_str("]")
-            }
-        }
-        )*
-    }
-}
-
-additional_slice_formatting!( Display, Debug, Binary, Octal, LowerHex, UpperHex, LowerExp, UpperExp, Pointer );
-
-/// Formats a byte-buffer as a series of concatanated hex-pairs.
-///
-/// Implements the following formatting traits:
-///		- Display, Debug, LowerHex - Will format in all letters in lowercase.
-///		- UpperHex - Will format in all letters in uppercase
-///
-/// # Examples
-/// ```
-/// use extfmt::*;
-///
-/// assert_eq!(format!("{}", Hexlify(&[122, 123, 134])), "7a7b86");
-/// assert_eq!(format!("{:X}", Hexlify(&[122, 123, 134])), "7A7B86");
-/// assert_eq!(format!("{:X}", Hexlify(&[122, 123, 134])), "7A7B86");
-/// ```
-pub struct Hexlify<'a>(pub &'a [u8]);
-
-impl<'a> LowerHex for Hexlify<'a> {
-	fn fmt(&self, f: &mut Formatter) -> Result {
-		for byte in self.0.iter() {
-			write!(f, "{:02x}", byte)?;
-		}
-
-		Ok(())
-	}
-}
-
-impl<'a> UpperHex for Hexlify<'a> {
-	fn fmt(&self, f: &mut Formatter) -> Result {
-		for byte in self.0.iter() {
-			write!(f, "{:02X}", byte)?;
-		}
-
-		Ok(())
-	}
-}
-
-impl<'a> Debug for Hexlify<'a> {
-	fn fmt(&self, f: &mut Formatter) -> Result {
-		<Self as LowerHex>::fmt(self, f)
-	}
-}
-
-impl<'a> Display for Hexlify<'a> {
-	fn fmt(&self, f: &mut Formatter) -> Result {
-		<Self as LowerHex>::fmt(self, f)
-	}
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         println!("{}", hexdump(&[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
+//     }
+// }
